c8cd2d45b49a435b715eaa62316bba2ec6974309
Transformer <111748842+goldenInteger@users.noreply.github.com>
2026-06-18 22:04:13 +0800

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        long long a;
+        cin >> a;
+        sum += a;
+        cout << sum << " ";
+    }
+    
+    return 0;
+}
